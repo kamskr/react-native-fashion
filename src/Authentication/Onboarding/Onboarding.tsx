bccffd95b9294e47b0d154ce29b3a7e17f8b6741
Kamil Sikora <kamil.sikora@sklodowscy.pl>
2020-07-18 14:22:29 +0200

    changed slider

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
+import Animated from "react-native-reanimated";
 
-import Slide from "./Slide";
+import Slide, { SLIDE_HEIGHT } from "./Slide";
 
-const { width, height } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
   },
   slider: {
-    height: 0.61 * height,
-    backgroundColor: "cyan",
+    height: SLIDE_HEIGHT,
     borderBottomRightRadius: 75,
   },
 
@@ -20,26 +20,42 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+const slides = [
+  { label: "Relaxed", color: "#BFEAF5" },
+  { label: "Playful", color: "#BEECC4" },
+  { label: "Excentric", color: "#FFE4D9" },
+  { label: "Funky", color: "#FFDDDD" },
+];
+
 const Onboarding = () => {
+  const x = useValue(0);
+  //TODO: scrollHandler useScrollHandler?
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
+  });
   return (
     <View style={styles.container}>
-      <View style={styles.slider}>
-        <ScrollView
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
           horizontal
           snapToInterval={width}
           decelerationRate="fast"
           showsHorizontalScrollIndicator={false}
           bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
         >
-          <Slide label="Relaxed" />
-          <Slide label="Playful" right />
-          <Slide label="Excentric" />
-          <Slide label="Funky" right />
-        </ScrollView>
-      </View>
+          {slides.map(({ label }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ label }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
       <View style={styles.footer}>
-        <View
-          style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "cyan" }}
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
         />
         <View
           style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}
--- src/Authentication/Onboarding/Slide.tsx
+++ src/Authentication/Onboarding/Slide.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { View, Text, Dimensions, StyleSheet } from "react-native";
 
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
+export const SLIDE_HEIGHT = 0.61 * height;
 const styles = StyleSheet.create({
-  constainer: {},
+  constainer: {
+    width,
+  },
+  titleContainer: {
+    height: 100,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 80,
+    lineHeight: 80,
+    fontFamily: "SFProText-Bold",
+    color: "white",
+    textAlign: "center",
+  },
 });
 
 interface SlideProps {
@@ -12,9 +26,16 @@ interface SlideProps {
 }
 
 const Slide = ({ label, right }: SlideProps) => {
+  const transform = [
+    { translateY: (SLIDE_HEIGHT - 100) / 2 },
+    { translateX: right ? width / 2 - 50 : -width / 2 + 50 },
+    { rotate: right ? "-90deg" : "90deg" },
+  ];
   return (
-    <View style={{ width }}>
-      <Text>{label}</Text>
+    <View style={styles.constainer}>
+      <View style={(styles.titleContainer, { transform })}>
+        <Text style={styles.title}>{label}</Text>
+      </View>
     </View>
   );
 };
